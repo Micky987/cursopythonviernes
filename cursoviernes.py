a26db069b9d5b2f34aaca41d44bb64db807cd5dd
miguelito <mickylaura77@gmail.com>
2024-04-26 20:57:05 -0400

    entrada de datos, operadores de asignacion

---
+++ cursoviernes.py
@@ -0,0 +1,12 @@
+mi_variable = input("escribe tu valor:")
+print(mi_variable)
+
+a=input("Primer Numero:") 
+b=input("Segundo Numero:")
+c=int(input("Primer Numero:"))
+a_transformado = int(a)
+b=int(b)
+resultado =a_transformado +b+c
+print(resultado)
+
+
---
+++ operadore.py
@@ -0,0 +1,23 @@
+print(17 % 3)  # resto
+print(3**2)
+print(24//9)   #parte entera
+
+
+a=5
+b=8
+a+=b    
+print (a)
+
+a=5
+b=4
+a-=b
+print (a) 
+
+
+
+print( 5 == 5 )
+print( 5 != 5 )
+print( 5 > 8 )
+print( 5 < 8 )
+print( 3 <= 10)
+print( 3 >= 10)
